d7c8c4681905dbba88d88c7ec55da22e9cc21f3c
Adriana Pereira <adripereira.adriana@gmail.com>
2019-10-10 13:26:34 +0100

    update after training new model - printing scores

--- NeuralTrainer.py
+++ NeuralTrainer.py
@@ -1,10 +1,12 @@
 import numpy as np
 import os
+import copy
 
 from sklearn.model_selection import KFold
 
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_absolute_error
 
 import keras
 from keras.models import Sequential, Model
@@ -18,7 +20,7 @@ class NeuralTrainer:
     hidden_size = 100
 
 
-    def __init__(self, maxlen, num_tags, word_index, embeddings, texts_to_eval, dumpPath):
+    def __init__(self, maxlen, num_tags, word_index, embeddings, texts_to_eval_dir, dumpPath):
         self.sequences = []
         self.maxlen = maxlen
         self.vocab_size = len(word_index)+1
@@ -26,9 +28,24 @@ class NeuralTrainer:
         self.num_measures = 1 + 3*(num_tags - 1)
         self.word_index = word_index
         self.embeddings = embeddings
-        self.texts_to_eval = texts_to_eval
+        self.texts_to_eval_dir = texts_to_eval_dir
         self.dumpPath = dumpPath
         self.model = None
+        self.classes = ['']*num_tags
+        self.read_tag_mapping()
+
+    def read_tag_mapping(self):
+        f = open('tag_mapping.txt', 'r', encoding='utf-8')
+        lines = f.readlines()
+        tags = {}
+        for mapping in lines:
+            if(mapping == ''):
+                continue
+            map = mapping.split('\t')
+            tags[int(map[1][0])-1] = map[0]
+        for i in range(0, self.num_tags):
+            self.classes[i] = tags[i]
+
 
     def decodeTags(self, tags):
         newtags = []
@@ -91,7 +108,7 @@ class NeuralTrainer:
 
         self.model = Model(input=input, output=[crf_tensor,dist_tensor])
         # print(self.model.summary())
-        self.model.compile(optimizer='adam', loss=[crf.loss_function,'mean_absolute_error'], metrics=[crf.accuracy])
+        self.model.compile(optimizer='adam', loss=[crf.loss_function,'mean_absolute_error'], metrics={'crf_1':[crf.accuracy], 'time_distributed_3':'accuracy'})
 
     def trainModel(self, x_train, y_train_class, y_train_dist, x_test, y_test_class, y_test_dist, unencodedY, testSet):
         monitor = EarlyStopping(monitor='loss', min_delta=0.001, patience=5, verbose=1, mode='auto')
@@ -106,14 +123,16 @@ class NeuralTrainer:
         # scores: loss, crf_loss, dist_loss, crf_acc, dist_acc
         print("%s: %.2f%%" % (self.model.metrics_names[3], scores[3] * 100))
         print("%s: %.2f%%" % (self.model.metrics_names[4], scores[4] * 100))
+        print("%s: %.2f%%" % (self.model.metrics_names[2], scores[2] * 100))
 
-        # self.printEvaluatedTexts(x_test, y_pred_class, y_pred_dist, testSet, self.texts_to_eval, self.dumpPath)
-        # spanEvalAt1 = self.spanEval(y_pred_class, y_pred_dist, unencodedY, 1.0)
-        # spanEvalAt075 = self.spanEval(y_pred_class, y_pred_dist, unencodedY, 0.75)
-        # spanEvalAt050 = self.spanEval(y_pred_class, y_pred_dist, unencodedY, 0.50)
-        # tagEval = self.tagEval(y_pred_class, y_pred_dist, unencodedY)
-        #
-        # return [scores[1], tagEval, spanEvalAt1, spanEvalAt075, spanEvalAt050]
+        self.write_evaluated_tests_to_file(x_test, y_pred_class, y_pred_dist, testSet, self.texts_to_eval_dir, self.dumpPath)
+        spanEvalAt1 = self.spanEval(y_pred_class, y_pred_dist, unencodedY, 1.0)
+        spanEvalAt075 = self.spanEval(y_pred_class, y_pred_dist, unencodedY, 0.75)
+        spanEvalAt050 = self.spanEval(y_pred_class, y_pred_dist, unencodedY, 0.50)
+        tagEval = self.tagEval(y_pred_class, unencodedY)
+        # distEval = self.distEval(y_pred_dist,y_test_dist,unencodedY)
+
+        return [scores[1], tagEval, spanEvalAt1, spanEvalAt075, spanEvalAt050]
 
     def crossValidate(self, X, Y, additionalX, additionalY, unencodedY):
         seed = 42
@@ -128,7 +147,7 @@ class NeuralTrainer:
         empty_avg = list(map(float,np.zeros(self.num_tags)))
         # nr_measures = 1 + 3*(self.num_tags-1)
         empty_corr = list(map(float,np.zeros(self.num_measures)))
-        cvscores = [0, [empty_avg, empty_avg, empty_avg], empty_corr, empty_corr, empty_corr]
+        cvscores = [0, [copy.deepcopy(empty_avg), copy.deepcopy(empty_avg), copy.deepcopy(empty_avg)], copy.deepcopy(empty_corr), copy.deepcopy(empty_corr), copy.deepcopy(empty_corr)]
 
         Y_class = Y[0] # Component identification
         Y_dist = Y[1] # Distances between related components
@@ -164,19 +183,19 @@ class NeuralTrainer:
             unencoded_Y = np.array(unencoded_Y)
 
             scores = self.trainModel(X_train, Y_train_class, Y_train_dist, X_test, Y_test_class,Y_test_dist, unencoded_Y, test)
-        #     cvscores = self.handleScores(cvscores, scores, n_folds)
+            cvscores = self.handleScores(cvscores, scores, n_folds)
             foldNumber += 1
             break
-        # print('Average results for the ten folds:')
-        # self.prettyPrintResults(cvscores)
-        # return cvscores
+        print('Average results for the ten folds:')
+        self.prettyPrintResults(cvscores)
+        return cvscores
 
     def handleScores(self, oldScores, newScores, nFolds):
         newAccuracy = oldScores[0] + (newScores[0] / nFolds)
         empty = list(map(float,np.zeros(self.num_tags)))
 
         #[precision[class], recall[class], f1[class]]
-        newTagScores = [empty, empty, empty]
+        newTagScores = [copy.deepcopy(empty), copy.deepcopy(empty), copy.deepcopy(empty)]
         #[acc, precision_c1, precision_c2, recall_c1, recall_c2, f1_c1, f1_c2]
         # nr_measures = 1 + 3*(self.num_tags-1)
 
@@ -201,13 +220,13 @@ class NeuralTrainer:
             #[acc, precision_c1, precision_c2, recall_c1, recall_c2, f1_c1, f1_c2]]  --> at 50
         return [newAccuracy, newTagScores, newSpanAt1Scores, newSpanAt075Scores, newSpanAt050Scores]
 
-    def tagEval(self, y_pred, unencodedY, ):
+    def tagEval(self, y_pred_class, unencodedY):
         i = 0
         precision = []
         recall = []
         f1 = []
         accuracy = []
-        for result in y_pred:
+        for result in y_pred_class:
             sequenceLength = len(np.trim_zeros(unencodedY[i]))
             result = np.resize(result, (sequenceLength, self.num_tags))
             classes = np.argmax(result, axis=1)
@@ -231,13 +250,26 @@ class NeuralTrainer:
         for scoreValue in score:
             score_sum += scoreValue[:self.num_tags]
 
-        print('(o,|,|)\t(i,claim,0)\t(i,premise,d)')
-        print(str(round(score_sum[0] / numTexts, 3)) + '\t' + str(
-            round(score_sum[1] / numTexts, 3)) + '\t' + str(np.round(score_sum[2:] / numTexts, 3)))
+        str_res = []
+        float_res = []
+        for i in range(0, self.num_tags):
+            str_res.append(str(round(score_sum[i] / numTexts, 3)))
+            float_res.append(round(score_sum[i] / numTexts, 4))
 
-        # return [round(score_sum[0] / numTexts, 4), round(score_sum[1] / numTexts, 4),
-        #         round(score_sum[2:] / numTexts, 4)]
-        return list(map(float,np.round(score_sum/numTexts,4)))
+        print('\t'.join(self.classes))
+        print('\t'.join(str_res))
+
+        return float_res
+
+    def distEval(self, y_pred_dist, y_test_dist, unencodedY):
+        i = 0
+        loss = []
+        for result in y_pred_dist:
+            sequenceLength = len(np.trim_zeros(unencodedY[i]))
+            loss.append(mean_absolute_error(y_test_dist[i][:sequenceLength], result[:sequenceLength]))
+            i += 1
+
+        print("Loss = %.3f% (+/- %.3f%)" % (np.mean(loss), np.std(loss)))
 
     def prettyPrintResults(self, scores):
 
@@ -245,6 +277,9 @@ class NeuralTrainer:
         #[acc, precision_c1, precision_c2, recall_c1, recall_c2, f1_c1, f1_c2],  --> at 100
         #[acc, precision_c1, precision_c2, recall_c1, recall_c2, f1_c1, f1_c2],  --> at 75
         #[acc, precision_c1, precision_c2, recall_c1, recall_c2, f1_c1, f1_c2]]  --> at 50
+
+        premise_index = self.classes.index('(I,premise)')
+        claim_index = self.classes.index('(I,claim)')
 
         print('Accuracy - ' + str(round(scores[0], 4)))
 
@@ -273,50 +308,44 @@ class NeuralTrainer:
         print('F1 for premises at ' + str(0.5) + ' - ' + str(round(scores[4][5], 3)))
         print('F1 for claims at ' + str(0.5) + ' - ' + str(round(scores[4][6], 3)))
 
-        print('@prettyPrintResults: scores[1] -- ', str(scores[1]))
-
         print('Precision')
-        print('(i,premise)' + '(o,|)   ' + '(i,claim)')
-        # print(str(round(score_sum[0] / numTexts, 3)) + '   ' + str(
-        #     round(score_sum[1] / numTexts, 3)) + '     ' + str(np.round(score_sum[2:] / numTexts, 3)))
+        print('\t'.join(self.classes))
         print(str(round(scores[1][0][0], 3)) + '   ' + str(round(scores[1][0][1], 3)) + '     ' + str(
             round(scores[1][0][2], 3)))
         print('Recall')
-        print('(i,premise)' + '(o,|)   ' + '(i,claim)')
+        print('\t'.join(self.classes))
         print(str(round(scores[1][1][0], 3)) + '   ' + str(round(scores[1][1][1], 3)) + '     ' + str(
             round(scores[1][1][2], 3)))
         print('F1')
-        print('(i,premise)' + '(o,|)   ' + '(i,claim)')
+        print('\t'.join(self.classes))
         print(str(round(scores[1][2][0], 3)) + '   ' + str(round(scores[1][2][1], 3)) + '     ' + str(
             round(scores[1][2][2], 3)))
 
-    def printEvaluatedTexts(self, x_test, y_pred, testSet, textList, dumpPath):
+    def write_evaluated_tests_to_file(self, x_test, y_pred_class, y_pred_dist, testSet, text_dir, dumpPath):
         invword_index = {v: k for k, v in self.word_index.items()}
         texts = []
 
         for i in range(0,len(x_test)):
             text = []
-            tags = y_pred[i]
+            tags = y_pred_class[i]
+            dists = y_pred_dist[i]
             j = 0
             for word in np.trim_zeros(x_test[i]):
-                text.append([invword_index[word], np.argmax(tags[j])])
+                text.append([invword_index[word], np.argmax(tags[j]), dists[j]])
                 j += 1
             texts.append(text)
 
-        fileList = os.listdir(textList)
+        fileList = os.listdir(text_dir)
         filenames = []
         for file in fileList:
             fileName = dumpPath + '/' + file
             filenames.append(fileName)
         filenames = [filenames[x] for x in testSet]
 
-        classes = ['(O,|,|)', '(I,Claim,0)', '(I,Premise,-1)', '(I,Premise,-2)', '(I,Premise,-3)', '(I,Premise,-4)', '(I,Premise,-5)',
-                    '(I,Premise,0)', '(I,Premise,-6)', '(I,Premise,-7)', '(I,Premise,-8)', '(I,Premise,-9)', '(I,Premise,-10)',
-                    '(I,Premise,-11)', '(I,Premise,-12)']
         for i in range(0, len(texts)):
             textFile = open(filenames[i], "w", encoding='utf-8')
             for token in texts[i]:
-                textFile.write(u'' + token[0] + ' ' + classes[token[1]] + '\n')
+                textFile.write(u'' + token[0] + ' ' + self.classes[token[1]] + ' ' + str(token[2]) + '\n')
 
     def spanCreator(self, unencodedY):
         spans = []
@@ -346,18 +375,18 @@ class NeuralTrainer:
                 trimmedSpans[predictedSpanStart] = predictedSpanEnd
         return trimmedSpans
 
-    def spanEval(self, y_pred, unencodedY, threshold):
+    def spanEval(self, y_pred_class, y_pred_dist, unencodedY, threshold):
         goldSpans = self.spanCreator(unencodedY)
         empty = list(map(float,np.zeros(self.num_tags)))
         i = 0
-        precision = empty
-        recall = empty
-        f1 = empty
-        predictedSpanTypes = empty
-        goldSpanTypes = empty
-        precisionCorrectSpans = empty
-        recallCorrectSpans = empty
-        for result in y_pred:
+        precision = copy.deepcopy(empty)
+        recall = copy.deepcopy(empty)
+        f1 = copy.deepcopy(empty)
+        predictedSpanTypes = copy.deepcopy(empty)
+        goldSpanTypes = copy.deepcopy(empty)
+        precisionCorrectSpans = copy.deepcopy(empty)
+        recallCorrectSpans = copy.deepcopy(empty)
+        for result in y_pred_class:
             sequenceLength = len(np.trim_zeros(unencodedY[i]))
             result = np.resize(result, (sequenceLength, self.num_tags))
             classes = np.argmax(result, axis=1)
@@ -400,28 +429,16 @@ class NeuralTrainer:
             if ((precision[i] + recall[i]) != 0):
                 f1[i] = 2 * ((precision[i] * recall[i]) / (precision[i] + recall[i]))
 
-        precision_premises = np.array(precision[2:])
-        recall_premises = np.array(recall[2:])
-        f1_premises = np.array(f1[2:])
-
+        premise_index = self.classes.index('(I,premise)')
+        claim_index = self.classes.index('(I,claim)')
         print('Accuracy at ' + str(threshold) + ' - ' + str(round(accuracy, 3)))
-        print('Precision for premises at ' + str(threshold) + ' - ' + str(np.round(precision_premises, 3)))
-        print('Precision for claims at ' + str(threshold) + ' - ' + str(round(precision[1], 3)))
-        print('Recall for premises at ' + str(threshold) + ' - ' + str(np.round(recall_premises, 3)))
-        print('Recall for claims at ' + str(threshold) + ' - ' + str(round(recall[1], 3)))
-        print('F1 for premises at ' + str(threshold) + ' - ' + str(np.round(f1_premises, 3)))
-        print('F1 for claims at ' + str(threshold) + ' - ' + str(round(f1[1], 3)))
+        print('Precision for premises at ' + str(threshold) + ' - ' + str(round(precision[premise_index], 3)))
+        print('Precision for claims at ' + str(threshold) + ' - ' + str(round(precision[claim_index], 3)))
+        print('Recall for premises at ' + str(threshold) + ' - ' + str(round(recall[premise_index], 3)))
+        print('Recall for claims at ' + str(threshold) + ' - ' + str(round(recall[claim_index], 3)))
+        print('F1 for premises at ' + str(threshold) + ' - ' + str(round(f1[premise_index], 3)))
+        print('F1 for claims at ' + str(threshold) + ' - ' + str(round(f1[claim_index], 3)))
 
-        ret = [round(accuracy, 4)]
-        for value in precision_premises:
-            ret.append(value)
-        ret.append(precision[1])
-        for value in recall_premises:
-            ret.append(value)
-        ret.append(recall[1])
-        for value in f1_premises:
-            ret.append(value)
-        ret.append(f1[1])
-
-        print('ret: ',len(ret))
+        ret = [round(accuracy, 4),round(precision[premise_index], 4),round(precision[claim_index], 4),round(recall[premise_index], 4),
+            round(recall[claim_index], 4),round(f1[premise_index], 4),round(f1[claim_index], 4)]
         return ret
